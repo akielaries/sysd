1669783bf313fb35d6405e5bbbfc970e0fe8ecde
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2024-01-05 02:30:18 -0700

    Create sysd.c

---
+++ sysd.c
@@ -0,0 +1,25 @@
+include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+
+int main() {
+    int file = open("/dev/i2c-1", O_RDWR);
+    if (file < 0) {
+        perror("Error opening I2C device");
+        exit(1);
+    }
+
+    unsigned char address = 0x27; // Replace with your LCD address
+    if (ioctl(file, I2C_SLAVE, address) < 0) {
+        perror("Error setting I2C address");
+        exit(1);
+    }
+
+    close(file);
+    return 0;
+}

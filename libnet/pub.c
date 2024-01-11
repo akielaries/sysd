df43167d5885c6429ebf8fe55a0051ab6be1f947
akielaries <akiel@akiel.org>
2024-01-11 20:09:36 +0000

    updates to pub main

--- libnet/pub.c
+++ libnet/pub.c
@@ -6,6 +6,7 @@
 int main() {
     const char *message = "Hello, World!";
     double num_d = 22.3;
+    float num_f = 99.345;
     int32_t num_i = 123;
     const char *ip_address = "192.168.255.2";
     const int port = 8888;
@@ -14,7 +15,7 @@ int main() {
 
 
     while (1) {
-        publish(socket, message, strlen(message) + 1);
+        publish(socket, message, strlen(message));
         publish(socket, &num_d, sizeof(num_d));
         publish(socket, &num_i, sizeof(num_i));
         sleep(5);

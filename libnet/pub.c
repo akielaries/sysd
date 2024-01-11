907a7f8964b1b738e70e580de7336e242f75a2a4
akielaries <akiel@akiel.org>
2024-01-11 00:33:54 +0000

    updating pub/sub

--- libnet/pub.c
+++ libnet/pub.c
@@ -1,16 +1,19 @@
 #include "net.h"
 #include <unistd.h>
+#include <string.h>
 
 int main() {
     const char *message = "Hello, World!";
+    double num = 22.3;
     const char *ip_address = "192.168.255.2";
     const int port = 8888;
 
     int socket = conn_init(ip_address, port);
 
+
     while (1) {
-        publish(socket, message);
-        publish(socket, "poobar?");
+        publish(socket, message, strlen(message));
+        publish(socket, &num, sizeof(num));
         sleep(5);
     }
 

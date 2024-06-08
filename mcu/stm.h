a5ed31442b59c9a75455bd7215bfa31e54ab5eaa
akielaries <akiel@akiel.org>
2024-06-08 07:37:35 +0000

    editing header

--- mcu/stm.h
+++ mcu/stm.h
@@ -1,6 +1,22 @@
 #ifndef __STM_H__
 #define __STM_H__
 
+#define SET_BIT(REG, BIT)     ((REG) |= (BIT))
+
+#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(BIT))
+
+#define READ_BIT(REG, BIT)    ((REG) & (BIT))
+
+#define CLEAR_REG(REG)        ((REG) = (0x0))
+
+#define WRITE_REG(REG, VAL)   ((REG) = (VAL))
+
+#define READ_REG(REG)         ((REG))
+
+#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+#define POSITION_VAL(VAL)     (__CLZ(__RBIT(VAL))) 
+
 // size of each register segment in the register map
 #define REG_SIZE 0x3FF
 

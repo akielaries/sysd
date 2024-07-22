c010378c50428cce49d299db2c2b20d15d7a360e
akielaries <akiel@akiel.org>
2024-07-22 05:29:49 +0000

    adding tests

--- sysd/network/proto_check.h
+++ sysd/network/proto_check.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+// crc-16 MODBUS: https://crccalc.com/
 uint16_t get_crc16(const uint8_t *data, size_t length);
 
 uint8_t get_checksum(const uint8_t *data, size_t length);
--- tests/test_serialize.cpp
+++ tests/test_serialize.cpp
@@ -16,7 +16,7 @@ TEST(SerializeTelemetryDataTest, ValidData) {
   uint8_t code = SYSD_CPU_TEMP;
   proto_datatypes_e dt = SYSD_TYPE_FLOAT;
   float temp = 32.3;
-  const char* dest = "192.168.1.20";
+  const char* dest = "192.168.1.10";
   uint32_t len = SYSD_MAX_MESSAGE_SIZE;
 
   proto_frame_t *data;
@@ -29,7 +29,35 @@ TEST(SerializeTelemetryDataTest, ValidData) {
   ASSERT_NE(data, nullptr);
   ASSERT_NE(data->buffer, nullptr);
   ASSERT_GT(len, 0);
+}
 
+TEST(SerializeTelemetryDataTest, DissectFrame) {
+  char valid_frame[] = {SYSD_START_BYTE_A, SYSD_START_BYTE_B, 
+                        0xC0, 0xA8, 0x01, 0x0A,
+                        SYSD_CPU_TEMP,
+                        SYSD_TYPE_FLOAT,
+                        0x42, 0x01, 0x33, 0x33,
+                        0xA8, 0xFD, 0xD4,
+                      };
+
+  uint8_t code = SYSD_CPU_TEMP;
+  proto_datatypes_e dt = SYSD_TYPE_FLOAT;
+  float temp = 32.3;
+  const char* dest = "192.168.1.10";
+  uint32_t len = SYSD_MAX_MESSAGE_SIZE;
+
+  proto_frame_t *data;
+  data = serialize(code,
+                   dt,
+                   &temp,
+                   dest,
+                   &len);
+  
+  for (uint8_t i = 0; i < data->length; i++) {
+    ASSERT_EQ(data->buffer[i], valid_frame[i]);
+  }
+
+  ASSERT_GT(len, 0);
 }
 
 int main(int argc, char **argv) {
